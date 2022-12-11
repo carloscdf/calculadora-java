bae0f7264c920c8636b36d70ccdeb92e4780c422
carloscdf <valdelicebatista60@gmail.com>
2022-12-10 21:15:44 -0300

    update

--- calculadora/src/main/java/com/calculadora/Calculadora.java
+++ calculadora/src/main/java/com/calculadora/Calculadora.java
@@ -9,8 +9,7 @@ public class Calculadora {
 
     public static void main(String[] args) {
         
-        
-
+       
          Scanner entrada = new Scanner(System.in);
                
         int op1, op2;
@@ -19,23 +18,34 @@ public class Calculadora {
         System.out.println("CALCULADORA\n");
        
         System.out.println("Escolha uma operação\nDigite um dos valores abaixo\n\n"
-                + "(1) Soma\n(2)Subtração\n(3)Multiplicação\n(4)Divisao");
+                + "(1)Soma\n(2)Subtração\n(3)Multiplicação\n(4)Divisao");
         
         System.out.print("\nEscolha a operação: ");
         operacao = entrada.nextInt(); 
         System.out.print("Digite um valor: ");
         op1 = entrada.nextInt();
         System.out.print("Digite outro valor: ");
-       op2= entrada.nextInt();
+        op2= entrada.nextInt();
 
         
        Operacoes op = new Operacoes(op1, op2);
 
+       
+       if (op1 < 0 && op2 <0) {
+         
+          op1 = Math.abs(op1); // tentei converter os númeors para positivo aqui mas consegui não
+          op2 = Math.abs(op2);
+           
+       }
+       
 
        switch( operacao) {
-           case 1 : System.out.println("Resultado da soma: " +op.Soma());
+           
+           case 1 : System.out.println("Resultado da soma: " + op.Soma());
            break;
            case 2:  System.out.println("Resultado da subtração: " + op.Subtracao());
+           break;
+           case 3:  System.out.println("Resultado da multiplicação: " + op.Multiplicacao());
            break;
            
        }
--- calculadora/src/main/java/com/calculadora/Operacoes.java
+++ calculadora/src/main/java/com/calculadora/Operacoes.java
@@ -1,11 +1,11 @@
 package com.calculadora;
 
 public class Operacoes {
-    
+
     private int op1;
     private int op2;
     private int resultado;
-   
+
     Operacoes(int op1, int op2) {
         this.op1 = op1;
         this.op2 = op2;
@@ -20,13 +20,20 @@ public class Operacoes {
         resultado = op1 - op2;
         return resultado;
     }
-    
+
     public int Multiplicacao() {
-        
-        
-        
-        return 0;
-        
+
+        while (op2 > 0) {
+
+            if ((op2 & 1) == 1) {
+
+                resultado += op1;
+
+            }
+            op1 = op1 << 1;
+            op2 = op2 >> 1;
+        }
+        return resultado;
     }
 
 }
